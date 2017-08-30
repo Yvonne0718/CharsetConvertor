bd0237a2add614389df834b9821cee5b41e613a3
Lee <chunxiaowan1@163.com>
2017-08-30 17:43:24 +0800

    自动滚动到日志最下方，异步UI刷新尚未完成

--- src/controller/ConvertController.java
+++ src/controller/ConvertController.java
@@ -2,9 +2,7 @@ package controller;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import convertor.Convertor;
 import filter.FileFilter;
--- src/ui/ConvertorFrame.java
+++ src/ui/ConvertorFrame.java
@@ -1,19 +1,14 @@
 package ui;
 
 import java.awt.Dimension;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.io.BufferedReader;
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
@@ -31,6 +26,8 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.filechooser.FileFilter;
 
 import controller.ConvertController;
@@ -47,7 +44,7 @@ public class ConvertorFrame extends JFrame implements ActionListener, FocusListe
     private JScrollPane jsp;
 
     private JComboBox tgcharsetlist = new JComboBox(
-            new Object[] { "UTF-8", "GBK", "ASCII", "unicode"});
+            new Object[] { "UTF-8", "GBK", "GB2312", "ASCII", "unicode"});
 
     private JMenu menu1 = new JMenu("Operate");
     private JMenuItem about = new JMenuItem("about");
@@ -59,10 +56,28 @@ public class ConvertorFrame extends JFrame implements ActionListener, FocusListe
     private JTextField tgType = new JTextField();
     private JButton query = new JButton("查询");
     private JButton enchangeStart = new JButton("开始转换");
-    //单独
     private final JTextArea taInfo = new JTextArea(getHelpMessage());
-
+    private CopyOnWriteArrayList<String> logqueue = new CopyOnWriteArrayList<String>();
+    
     private void initFrame() {
+        this.taInfo.getDocument().addDocumentListener(new DocumentListener() {
+            
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                
+            }
+            
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                ConvertorFrame.this.taInfo.setCaretPosition(ConvertorFrame.this.taInfo.getText().length());
+                
+            }
+            
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                
+            }
+        });
         initMenu();
         initTopPanel();
         initCenterPanel();
@@ -147,76 +162,20 @@ public class ConvertorFrame extends JFrame implements ActionListener, FocusListe
         add(jsp, "Center");
     }
 
-    @Override
-    public void actionPerformed(ActionEvent event) {
-        Object ob = event.getSource();
-        if (ob == this.about) {
-            System.out.println("about");
-            // JFrame jf = new JFrame("about");
-            // jf.setSize(200, 100);
-            // jf.setVisible(true);
-        } else if (ob.equals(this.exitMenu)) {
-            System.exit(0);
-        } else if (ob.equals(this.chooseflo)) {
-            JFileChooser jfc = new JFileChooser();
-            jfc.setDialogTitle("请选择需要转换的文件夹\\文件");
-            jfc.setFileFilter(this.dirctoryFileFilter);
-            jfc.setMultiSelectionEnabled(true);
-            jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-            jfc.setApproveButtonText("确定");
-            jfc.showOpenDialog(this);
-            // TODO 记住用户选择的文件
-            File[] files = jfc.getSelectedFiles();
-            this.converController.setTempFile(files);
-            if (files.length != 0) {// 等于一个的时候可能是一个文件夹也可能是一个文件
-                String tgdirpath;
-                if (files.length == 1)
-                    tgdirpath = files[0].isDirectory() ? files[0].getPath()
-                            : files[0].getParent();
-                else
-                    tgdirpath = files[0].getParent();
-                this.tgdir.setText(tgdirpath);
-                this.tgdir_temp = this.tgdir.getText();
-
-            }
-
-        } else if (ob.equals(this.tgcharsetlist)) {
-            this.converController
-                    .setTgEncoding((String) this.tgcharsetlist.getSelectedItem());
-        } else if (ob.equals(this.hivecb)) {
-            // System.out.println("hivecb");
-            this.converController.setHive(this.hivecb.isSelected());
-        } else if (ob.equals(this.query)) {
-            // System.out.println("query");
-            this.checkAdtype();
-            this.checkTgdir();
-            this.checkTgtype();
-            this.converController.query(true);
-            // int height=10;
-            // Point p = new Point();
-            // System.out.println("count : " + this.taInfo.getLineCount());
-            // p.setLocation(0,this.taInfo.getLineCount()*height);
-            // this.jsp.setLocation(p);
-        } else if (ob.equals(this.enchangeStart)) {
-            this.checkAdtype();
-            this.checkTgdir();
-            this.checkTgtype();
-            this.converController.encodingConvertStart();
-        }
-    }
-
     public ConvertorFrame() {
         super("Encoding Convertor");
         logger = new Logger() {
             public void log(String message) {
-                ConvertorFrame.this.taInfo.append(message + "\n");
+                logqueue.add(message);
             }
 
             public void space() {
-                ConvertorFrame.this.taInfo.append("\n");
-                ConvertorFrame.this.taInfo.append(
-                        "-----------------------------------------------------------------------------------------");
-                ConvertorFrame.this.taInfo.append("\n");
+                synchronized (taInfo) {
+                    ConvertorFrame.this.taInfo.append("\n");
+                    ConvertorFrame.this.taInfo.append(
+                            "-----------------------------------------------------------------------------------------");
+                    ConvertorFrame.this.taInfo.append("\n");
+                }
             }
         };
         converController = ConvertController.getConvertController();
@@ -239,24 +198,9 @@ public class ConvertorFrame extends JFrame implements ActionListener, FocusListe
 
         ConvertorFrame mainframe = new ConvertorFrame();
         URL url = ConvertorFrame.class.getResource("/img/motug.png");
-        Image image = ImageIO.read(url);
+        BufferedImage image = ImageIO.read(url);
         mainframe.setIconImage(image);
-    }
-
-    @Override
-    public void focusGained(FocusEvent e) {
-    }
-
-    @Override
-    public void focusLost(FocusEvent e) {
-        Object r = e.getSource();
-        if (r.equals(this.tgdir)) {
-            this.checkTgdir();
-        } else if (r.equals(this.adType)) {
-            this.checkAdtype();
-        } else if (r.equals(this.tgType)) {
-            this.checkTgtype();
-        }
+        new Thread(mainframe.new LogInfo()).start();
     }
 
     private void checkTgdir() {
@@ -282,20 +226,32 @@ public class ConvertorFrame extends JFrame implements ActionListener, FocusListe
 
     private String getHelpMessage() {
         StringBuffer helpMessage = new StringBuffer();
-        try {
-            String path = ConvertorFrame.class.getResource("/help.txt").getPath();
-            InputStream in = new FileInputStream(new File(path));
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                helpMessage.append(line + "\n");
-            }
-            reader.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            URL url = ConvertorFrame.class.getResource("/help/help.txt");
+//            System.out.println(url);
+//            InputStream in = new FileInputStream(new File(url.getPath()));
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                helpMessage.append(line + "\n");
+//            }
+//            reader.close();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return helpMessage.toString();
+        helpMessage.append("编码转换器简介：\n")
+        .append(" 对用户选中的文件进行字符编码转换，自动检测选中文件的原编码格式，支持多种格式转换。\n")
+        .append("操作步骤：\n")
+        .append(" 1. 选择需要转换的文件（若选择文件夹将自动扫描文件夹及子文件夹下的所有文件）\n")
+        .append(" 2. 选择需要转换的目标字符编码格式。\n")
+        .append(" 3. 输入需要过滤的文件类型（可选）。例：所有的 \".txt\"文本文件不需要转换，则输入\n")
+        .append("    文件的后缀 \"txt\",若有多种需要过滤的格式则使用逗号\",\"隔开\n")
+        .append(" 4. 输入目标文件格式（可选）。和过滤文件类型的要求一样\n")
+        .append(" 5. 选择是否过滤以 \".\"作为文件名开头的隐藏文件\n")
+        .append(" 6. 点击\"开始转换\");");
         return helpMessage.toString();
     }
 
@@ -305,8 +261,96 @@ public class ConvertorFrame extends JFrame implements ActionListener, FocusListe
         }
 
         public String getDescription() {
-            return "Dirctory";
+            return "Directory";
         }
     };
+    
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        Object ob = event.getSource();
+        if (ob == this.about) {
+            System.out.println("about");
+            // JFrame jf = new JFrame("about");
+            // jf.setSize(200, 100);
+            // jf.setVisible(true);
+        } else if (ob.equals(this.exitMenu)) {
+            System.exit(0);
+        } else if (ob.equals(this.chooseflo)) {
+            JFileChooser jfc = new JFileChooser();
+            jfc.setDialogTitle("请选择需要转换的文件夹\\文件");
+            jfc.setFileFilter(this.dirctoryFileFilter);
+            jfc.setMultiSelectionEnabled(true);
+            jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+            jfc.setApproveButtonText("确定");
+            jfc.showOpenDialog(this);
+            // TODO 记住用户选择的文件
+            jfc.changeToParentDirectory();
+            File[] files = jfc.getSelectedFiles();
+            this.converController.setTempFile(files);
+            if (files.length != 0) {// 等于一个的时候可能是一个文件夹也可能是一个文件
+                String tgdirpath;
+                if (files.length == 1)
+                    tgdirpath = files[0].isDirectory() ? files[0].getPath()
+                            : files[0].getParent();
+                else
+                    tgdirpath = files[0].getParent();
+                this.tgdir.setText(tgdirpath);
+                this.tgdir_temp = this.tgdir.getText();
+            }
+        } else if (ob.equals(this.tgcharsetlist)) {
+            this.converController
+                    .setTgEncoding((String) this.tgcharsetlist.getSelectedItem());
+        } else if (ob.equals(this.hivecb)) {
+            this.converController.setHive(this.hivecb.isSelected());
+        } else if (ob.equals(this.query)) {
+            this.checkAdtype();
+            this.checkTgdir();
+            this.checkTgtype();
+            this.converController.query(true);
+        } else if (ob.equals(this.enchangeStart)) {
+            this.checkAdtype();
+            this.checkTgdir();
+            this.checkTgtype();
+            this.converController.encodingConvertStart();
+        }
+    }
+    
+    @Override
+    public void focusGained(FocusEvent e) {
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        Object r = e.getSource();
+        if (r.equals(this.tgdir)) {
+            this.checkTgdir();
+        } else if (r.equals(this.adType)) {
+            this.checkAdtype();
+        } else if (r.equals(this.tgType)) {
+            this.checkTgtype();
+        }
+    }
+    
+    class LogInfo implements Runnable{
+
+//        String message;
+        /* 
+         * @see java.lang.Runnable#run()
+         */
+        @Override
+        public void run() {
+            while(true){
+                for(int i=0;i<logqueue.size();i++){
+                    taInfo.append(logqueue.get(i) + "\n");
+                    logqueue.remove(i);
+                    taInfo.repaint();
+                    jsp.repaint();
+//                    jsp.getParent().repaint();
+                    
+                }
+            }
+        
+        }
+    }
 
 }
